0ecdfdd3187d13b42fc8d23fe6749092f61a2b62
Hadis010 <hafsadini001@gmail.com>
2026-05-12 11:00:14 +0200

    test(molecule): add testinfra tests for maildev service and ports

--- molecule/default/tests/test_app.py
+++ molecule/default/tests/test_app.py
@@ -24,3 +24,16 @@ def test_nginx_running(host):
 def test_nginx_proxy(host):
     response = host.run("curl -s http://localhost:80/todos")
     assert response.rc == 0
+
+def test_maildev_running(host):
+    service = host.service("maildev")
+    assert service.is_running
+    assert service.is_enabled
+
+def test_maildev_smtp_port(host):
+    port = host.socket("tcp://0.0.0.0:1025")
+    assert port.is_listening
+
+def test_maildev_web_port(host):
+    port = host.socket("tcp://0.0.0.0:1080")
+    assert port.is_listening
